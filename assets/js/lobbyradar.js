bf7fe546989eb3ce0ec0d28b21f30bf1d00a1225
Felix <ff@dreimorgen.com>
2015-01-30 10:12:25 +0100

    add RWD scrolling for result-list

--- assets/js/lobbyradar.js
+++ assets/js/lobbyradar.js
@@ -22,6 +22,30 @@ $( ".logo-name" ).click(function() {
   }
 });
 
+// http://stackoverflow.com/questions/16437182/issue-with-a-scrollable-div-on-ipad
+$('body').on('touchmove','.scrollable',function(e) {
+  var tot = 0;
+  $(this).children('li:visible').each(function() { tot += $(this).height(); });
+  if(tot > $(this).innerHeight()) {
+    e.stopPropagation();
+  }
+});
+
+//uses body because jquery on events are called off of the element they are
+//added to, so bubbling would not work if we used document instead.
+$('body').on('touchstart','.scrollable',function(e) {
+    if (e.currentTarget.scrollTop === 0) {
+        e.currentTarget.scrollTop = 1;
+    } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
+        e.currentTarget.scrollTop -= 1;
+    }
+});
+
+//prevents preventDefault from being called on document if it sees a scrollable div
+$('body').on('touchmove','.scrollable',function(e) {
+    e.stopPropagation();
+});
+
 //    $("#joschka").click(function () {
 //           // $(".result-list").hide("slide", { direction: "left" }, 1000);
 //           $(".result-list").animate({height:"toggle",opacity:"toggle", easing: "easeOutQuint"},500);
