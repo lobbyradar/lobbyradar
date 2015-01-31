4ffb6419421ee989155e9a74cad7245260e3e586
Michael Kreil <github@michael-kreil.de>
2015-01-31 18:32:56 +0100

    optimizing tile generator

--- lobbynetwork/test.js
+++ lobbynetwork/test.js
@@ -73,7 +73,7 @@ force.on('tick', function () {
 	if (interation >= maxIterations) {
 		force.stop();
 		savePositions();
-		saveTiles(2);
+		saveTiles(8);
 		return
 	}
 
@@ -107,7 +107,8 @@ function saveTiles(maxDepth) {
 	var todos = [];
 
 	todos.push(function () {
-		render(0,0,0,nodes,links)
+		render(0,0,0,nodes,links);
+		//render(0,0,0,[],links)
 	})
 
 	nextTodo();
@@ -131,20 +132,46 @@ function saveTiles(maxDepth) {
 
 		ensureFolder(foldername);
 
-		console.log('write "'+filename+'"');
+		if (z0 < maxDepth) {
+			prepareSubTile(x0*2+0, y0*2+0, z0+1);
+			prepareSubTile(x0*2+1, y0*2+0, z0+1);
+			prepareSubTile(x0*2+0, y0*2+1, z0+1);
+			prepareSubTile(x0*2+1, y0*2+1, z0+1);
+		}
 
 		var scale = imageSize/(tileSize*Math.pow(2,z0));
 		var cx = imageSize*(1/2 - x0/Math.pow(2,z0));
 		var cy = imageSize*(1/2 - y0/Math.pow(2,z0));
 
-		console.log(scale, cx, cy);
-		//process.exit();
+		function prepareSubTile(xn, yn, zn) {
+			var radius = 0.8*imageSize/Math.pow(2,zn);
+			var radius2 = sqr(radius);
+			var cx = imageSize*(-1/2 + (+1/2 + xn)/Math.pow(2,zn));
+			var cy = imageSize*(-1/2 + (+1/2 + yn)/Math.pow(2,zn));
 
-		if (z0 < maxDepth) {
-			todos.push(function () { render(x0*2+0, y0*2+0, z0+1, nodes, links) });
-			todos.push(function () { render(x0*2+1, y0*2+0, z0+1, nodes, links) });
-			todos.push(function () { render(x0*2+0, y0*2+1, z0+1, nodes, links) });
-			todos.push(function () { render(x0*2+1, y0*2+1, z0+1, nodes, links) });
+			var newNodes = nodes.filter(function (node) {
+				return Math.sqrt(sqr(cx-node.x)+sqr(cy-node.y)) < (radius+node.r)
+			})
+
+			var newLinks = links.filter(function (link) {
+				var n1 = link.source;
+				var n2 = link.target;
+				lx = (n1.x-n2.x)*a + n2.x;
+				ly = (n1.y-n2.y)*a + n2.y;
+				var ldx = (n1.x - n2.x);
+				var ldy = (n1.y - n2.y);
+				var lx0 = (n2.x - cx);
+				var ly0 = (n2.y - cy);
+
+				var a = -(lx0*ldx + ly0*ldy)/(ldx*ldx + ldy*ldy);
+				if (a < 0) a = 0;
+				if (a > 1) a = 1;
+				var d2 = sqr(ldx*a + lx0) + sqr(ldy*a + ly0);
+
+				return d2 < radius2;
+			});
+
+			todos.push(function () { render(xn, yn, zn, newNodes, newLinks) });
 		}
 
 		var strokeOpacity = 0.7;
@@ -229,89 +256,8 @@ function saveTiles(maxDepth) {
 			fs.mkdirSync(folder);
 		}
 	}
+
+	function sqr(v) {
+		return v*v;
+	}
 }
-
-function writePNG(filename) {
-	console.log('write "'+filename+'"');
-
-	var size = imageSize/scale;
-	var cx = size/2;
-	var cy = size/2;
-
-	var strokeOpacity = 0.7;
-	var strokeWidth = (0.5/scale);
-	if (strokeWidth < 1) {
-		strokeOpacity *= strokeWidth;
-		strokeWidth = 1;
-	}
-
-	var commands = [
-		'push graphic-context',
-		'viewbox 0 0 '+size+' '+size,
-			'push graphic-context',
-				'fill white',
-				'rectangle 0 0 '+size+' '+size,
-			'pop graphic-context',
-			'push graphic-context',
-				'stroke "rgb(20,59,82)"',
-				'fill none',
-				'stroke-opacity '+strokeOpacity,
-				'stroke-width '+strokeWidth,
-				'stroke-antialias 1',
-				links.map(line).join('\n'),
-			'pop graphic-context',
-			'push graphic-context',
-				'fill "#fa7d18"',
-				nodes.filter(function (n) { return n.type == 'person' }).map(circle).join('\n'),
-			'pop graphic-context',
-			'push graphic-context',
-				'fill "#a3db19"',
-				nodes.filter(function (n) { return n.type != 'person' }).map(circle).join('\n'),
-			'pop graphic-context',
-		'pop graphic-context'
-	].join('\n');
-
-	//console.log(commands);
-
-
-	function circle(node) {
-		return 'circle ' + [
-			(node.x)/scale + cx,
-			(node.y)/scale + cy,
-			(node.x + node.r)/scale + cx,
-			(node.y)/scale + cy
-		].join(',');
-	}
-
-
-	function line(link) {
-		var n1 = link.source;
-		var n2 = link.target;
-		return 'line ' + [
-			n1.x/scale + cx,
-			n1.y/scale + cy,
-			n2.x/scale + cx,
-			n2.y/scale + cy
-		].join(',');
-	}
-
-	
-	var t = gm(new Buffer(commands));
-	
-	t.addSrcFormatter(function (a) {
-		a.forEach(function (e,i) { a[i] = 'MVG:'+e });
-	});
-	t.in('-size');
-	t.in(size+'x'+size);
-	t.in('-depth');
-	t.in('16');
-
-	t.write(filename, function (err) {
-		if (err) {
-			console.error(err);
-		} else {
-			console.log(filename);
-		}
-	});
-}
-
