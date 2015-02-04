c7167f2191a5867a268b87c82eaa3f256b7e6c94
Michael Kreil <github@michael-kreil.de>
2015-02-05 00:02:41 +0100

    adding netviz

--- assets/js/networkviz.js
+++ assets/js/networkviz.js
@@ -1,78 +1,93 @@
-function init() {
+var NetworkViz = (function () {
 
 	var f = 32768;
 
 	var positions = {};
-	var keys = Object.keys(node_positions);
-	var n = node_positions[keys[0]].length;
-	for (var i = 0; i < n; i++) {
-		var entry = {};
-		keys.forEach(function (key) {
-			entry[key] = node_positions[key][i];
+	var initialized = false;
+
+	function init() {
+		var keys = Object.keys(node_positions);
+		var n = node_positions[keys[0]].length;
+		for (var i = 0; i < n; i++) {
+			var entry = {};
+			keys.forEach(function (key) {
+				entry[key] = node_positions[key][i];
+			})
+			entry.x += f/2;
+			entry.y += f/2;
+			positions[entry.id] = entry;
+		}
+		node_positions = positions;
+
+		var projection = {
+			project: function (latlng) {
+				return L.point(latlng.lng/f, -latlng.lat/f);
+			},
+			unproject: function (point) {
+				return L.latLng(-point.y*f, point.x*f);
+			}
+		};
+
+		var crs = L.extend({}, L.CRS, {
+			projection: projection,
+			transformation: new L.Transformation(1, 0, 1, 0),
+			scale: function (zoom) {
+				return 256*Math.pow(2, zoom);
+			}
+		});
+
+		$('#networkviz').css({height:'100%'});
+
+		var map = L.map('networkviz', {
+			minZoom: 0,
+			maxZoom: 7,
+			zoom: 3,
+			center: [-0.5*f,0.5*f],
+			maxBounds: L.latLngBounds([-f,0],[0,f]),
+			crs: crs,
+			zoomAnimation: false
+		});
+
+		var layer = L.tileLayer('http://lobbyradar.opendatacloud.de/lobbynetwork/tiles/{z}/{y}/{x}.png', {
+			minZoom: 0,
+			maxZoom: 7,
+			maxNativeZoom: 7,
+			tileSize: 256,
+			zoomOffset: 0,
+			attribution: '<a href="https://events.ccc.de/congress/2013/">31C3</a> | Improve the code on <a href="https://github.com/OpenDataCity/31c3-map">GitHub</a>!',
+			noWrap: true
 		})
-		entry.x += f/2;
-		entry.y += f/2;
-		positions[entry.id] = entry;
+		
+		layer.addTo(map);
+
+		var labelLayer = L.layerGroup();
+		labelLayer.addTo(map);
 	}
-	node_positions = positions;
-	console.log(positions);
+	/*
 
-	var projection = {
-		project: function (latlng) {
-			return L.point(latlng.lng/f, -latlng.lat/f);
-		},
-		unproject: function (point) {
-			return L.latLng(-point.y*f, point.x*f);
-		}
-	};
+		setTimeout(function () {
+			//showEntity('54bd3c8d8b934da063412627') // Axel Voss
+			showEntity('54bd3c748b934da06340f4c4') // CSU
+		}, 1000);
+*/
 
-	var crs = L.extend({}, L.CRS, {
-		projection: projection,
-		transformation: new L.Transformation(1, 0, 1, 0),
-		scale: function (zoom) {
-			return 256*Math.pow(2, zoom);
-		}
-	});
-
-	var map = L.map('map', {
-		minZoom: 0,
-		maxZoom: 7,
-		zoom: 3,
-		center: [-0.5*f,0.5*f],
-		maxBounds: L.latLngBounds([-f,0],[0,f]),
-		crs: crs,
-		zoomAnimation: false
-	});
-
-	var layer = L.tileLayer('http://lobbyradar.opendatacloud.de/lobbynetwork/tiles/{z}/{y}/{x}.png', {
-		minZoom: 0,
-		maxZoom: 7,
-		maxNativeZoom: 7,
-		tileSize: 256,
-		zoomOffset: 0,
-		attribution: '<a href="https://events.ccc.de/congress/2013/">31C3</a> | Improve the code on <a href="https://github.com/OpenDataCity/31c3-map">GitHub</a>!',
-		noWrap: true
-	})
-	
-	layer.addTo(map);
-
-	var labelLayer = L.layerGroup();
-	labelLayer.addTo(map);
-
-	setTimeout(function () {
-		//showNode('54bd3c8d8b934da063412627') // Axel Voss
-		showNode('54bd3c748b934da06340f4c4') // CSU
-	}, 1000);
-
-	function showNode(id) {
+	function panToEntity(id) {
+		if (!initialized) init();
 		if (!node_positions[id]) return;
+
 		var node = node_positions[id];
 		var zoom = Math.round(13 - Math.log(node.r)/Math.log(2));
 		map.setView(L.latLng(-node.y, node.x), zoom, {animate:true})
-		showLabel(node);
+		//showLabel(node);
 	}
+/*
+		function showLabel(node) {
 
-	function showLabel(node) {
-
+		}
 	}
-}
+	*/
+
+	return {
+		panToEntity: panToEntity
+	}
+})()
