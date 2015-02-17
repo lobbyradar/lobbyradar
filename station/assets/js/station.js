b6b7fda2fa985d7b12bfbee8ebf63874c07a7686
ffalt <ffalt@datenfreunde.com>
2015-02-17 14:41:49 +0100

    organisation mit "type:organisation" angelegt anstelle "type:entity"

--- station/assets/js/station.js
+++ station/assets/js/station.js
@@ -1142,6 +1142,7 @@ var typedEntityEditCtrl = function ($scope, $state, $stateParams, api, fields, t
 			}
 		);
 	}
+	console.log('type', type);
 
 	fields.list({mode: mode}, function (data) {
 			if (data.error) return reportServerError($scope, data.error);
@@ -1279,7 +1280,7 @@ app.controller('PersonEditCtrl', function ($scope, $state, $stateParams, persons
 });
 
 app.controller('OrganisationEditCtrl', function ($scope, $state, $stateParams, organisations, fields, tags) {
-	typedEntityEditCtrl($scope, $state, $stateParams, organisations, fields, tags, 'organisation', 'organisations', 'Organisation');
+	typedEntityEditCtrl($scope, $state, $stateParams, organisations, fields, tags, 'entity', 'organisations', 'Organisation');
 });
 
 app.controller('LoginCtrl', function ($scope, $state, $stateParams, $resource, $rootScope, auth) {
