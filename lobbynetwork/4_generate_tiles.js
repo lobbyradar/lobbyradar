15e0db9d6be6dedb1b86446ab58a4c27211991d7
Michael Kreil <github@michael-kreil.de>
2015-02-25 17:54:04 +0100

    export positions

--- lobbynetwork/4_generate_tiles.js
+++ lobbynetwork/4_generate_tiles.js
@@ -15,35 +15,6 @@ var links = data.links;
 
 saveTiles(maxTileLevel);
 
-
-function exportPositions() {
-	console.log('export positions');
-	
-	var positions = nodes.map(function (node) {
-		return {
-			id: node.id,
-			name: node.name,
-			type: node.type,
-			r: Math.round(node.r*4),
-			x: Math.round(node.x*4),
-			y: Math.round(node.y*4)
-		}
-	});
-
-	positions.sort(function (a,b) {
-		return (a.id < b.id) ? -1 : 1;
-	})
-
-	var result = {};
-	Object.keys(positions[0]).forEach(function (key) {
-		result[key] = positions.map(function (entry) { return entry[key] });
-	});
-
-	result = 'var node_positions = '+JSON.stringify(result)+';';
-	fs.writeFileSync('./node_positions.js', result, 'utf8');
-}
-
-
 function saveTiles(maxDepth) {
 	var maxProcesses = 4;
 	var emptyImageBuffer;
--- lobbynetwork/update_network.sh
+++ lobbynetwork/update_network.sh
@@ -1,3 +1,4 @@
 node 1_get_data.js
 node 2_update_layout.js
-node 3_generate_tiles.js
+node 3_export_positions.js
+node 4_generate_tiles.js
