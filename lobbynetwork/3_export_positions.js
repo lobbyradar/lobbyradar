96577662389c3d6fd8a0dfba1a764d813a44c840
Michael Kreil <github@michael-kreil.de>
2015-03-11 16:31:51 +0100

    exporting edges

--- lobbynetwork/3_export_positions.js
+++ lobbynetwork/3_export_positions.js
@@ -23,9 +23,41 @@ function exportPositions() {
 
 	positions.sort(function (a,b) {
 		return (a.id < b.id) ? -1 : 1;
+	});
+
+	console.log('   prepare id lookup');
+	var lookup = {};
+	positions.forEach(function (node, index) {
+		lookup[node.id] = index;
 	})
 
-	var result = {};
+	console.log('   generate edges');
+	var edges = [];
+	links.forEach(function (link) {
+		var i1 = lookup[link.source.id];
+		var i2 = lookup[link.target.id];
+		if (i1 === undefined) return;
+		if (i2 === undefined) return;
+
+		edges.push((i1 < i2) ? [i1,i2] : [i2,i1]);
+	})
+
+	console.log('   sort edges');
+	edges.sort(function (a,b) {
+		if (a[0] != b[0]) return a[0] - b[0];
+		return a[1] - b[1];
+	})
+	
+	console.log('   remove duplicated edges');
+	var lastHash = '';
+	edges = edges.filter(function (edge) {
+		var hash = edge.join('_');
+		if (hash == lastHash) return false;
+		lastHash = hash;
+		return true;
+	})
+
+	var result = {nodes:{},edges:edges};
 	Object.keys(positions[0]).forEach(function (key) {
 		result[key] = positions.map(function (entry) { return entry[key] });
 	});
