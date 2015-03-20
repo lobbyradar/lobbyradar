28f059c95b342769665e4c05f5b4ee5d08b0a5f9
yetzt <git@yetzt.me>
2015-03-20 17:18:37 +0100

    fix bash scripts

--- lobbynetwork/update_network_fast.sh
+++ lobbynetwork/update_network_fast.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-NODE=`which node`
+if [ -e "$HOME/.bashrc" ]; then source $HOME/.bashrc; fi;
+if [ -e "$HOME/.nvm/nvm.sh" ]; then source $HOME/.nvm/nvm.sh; fi;
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR;
-$NODE $DIR/1_get_data.js;
-$NODE $DIR/2_update_layout.js 100;
-$NODE $DIR/3_export_positions.js;
-$NODE $DIR/4_generate_tiles.js 1;
-
+/usr/bin/env node $DIR/1_get_data.js;
+/usr/bin/env node $DIR/2_update_layout.js 100;
+/usr/bin/env node $DIR/3_export_positions.js;
+/usr/bin/env node $DIR/4_generate_tiles.js 1;
