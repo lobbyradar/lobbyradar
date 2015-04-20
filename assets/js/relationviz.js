91fb1d85a03ff4ba6fff2b951583f0ecd415ea90
Letty <dittrich.katja@googlemail.com>
2015-04-20 18:15:59 +0200

    dont use broken relations

--- assets/js/relationviz.js
+++ assets/js/relationviz.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 		console.log(data);
 
 		links = data.result.map(function (relation) {
-			console.log(relation);
+			if (relation.entities < 2) return;
 			var source = relation.entities[0];
 			var target = relation.entities[1];
 
