4666688b4e708fb6c9cb68498891faac28015d3a
Felix <ff@dreimorgen.com>
2015-04-24 17:39:36 +0200

    add articles from zdf via xml-api; cleanup

--- assets/js/articles.js
+++ assets/js/articles.js
@@ -1,88 +1,59 @@
-// $.ajax({
-//     url: 'http://www.zdf.de/api/v2/content/p12:37951336',
-//     dataType: 'jsonp',
-//     contentType: "text/xml",
-//     headers: { 'Access-Control-Allow-Origin': '*' },
-//     crossDomain: true,
-//     success: function(data){
-//         // Extract relevant data from XML
-//         console.log(data);
-//         // var xml_node = $('treffer',data);
-//         // console.log( xml_node.find('Teaser > dachzeile').text() );
-//     },
-//     error: function(data){
-//         console.log('Error loading XML data');
-//     }
-// });
-
-// function xml2json(xml) {
-//   try {
-//     var obj = {};
-//     if (xml.children.length > 0) {
-//       for (var i = 0; i < xml.children.length; i++) {
-//         var item = xml.children.item(i);
-//         var nodeName = item.nodeName;
-
-//         if (typeof (obj[nodeName]) == "undefined") {
-//           obj[nodeName] = xml2json(item);
-//         } else {
-//           if (typeof (obj[nodeName].push) == "undefined") {
-//             var old = obj[nodeName];
-
-//             obj[nodeName] = [];
-//             obj[nodeName].push(old);
-//           }
-//           obj[nodeName].push(xml2json(item));
-//         }
-//       }
-//     } else {
-//       obj = xml.textContent;
-//     }
-//     return obj;
-//   } catch (e) {
-//       console.log(e.message);
-//   }
-// }
-
-//  function getZDFArticles(data){
-//     alert('hi!');
-//     var obj = $.parseJSON(data);
-//     console.log(obj);
-// }
-
-// $.get( "http://www.zdf.de/api/v2/content/p12:37951336", function( data ) {
-//     var jsonObj = $.xml2json(data);
-//   console.log(jsonObj)
-//   alert( "Load was performed." );
-// });
-
-
-var createCORSRequest = function(method, url) {
+function createCORSRequest(method, url) {
   var xhr = new XMLHttpRequest();
+  
   if ("withCredentials" in xhr) {
-    // Most browsers.
+    // XHR for Chrome/Firefox/Opera/Safari.
     xhr.open(method, url, true);
   } else if (typeof XDomainRequest != "undefined") {
-    // IE8 & IE9
+    // XDomainRequest for IE.
     xhr = new XDomainRequest();
     xhr.open(method, url);
   } else {
-    // CORS not supported.
-    xhr = null;
+    xhr = null; // CORS not supported.
   }
   return xhr;
-};
+}
 
-var url = 'http://module.zdf.de/api/v2/content/p12:37951336';
-var method = 'GET';
-var xhr = createCORSRequest(method, url);
 
-xhr.onload = function() {
-  console.log(xhr);
-};
+// Make the actual CORS request.
+function makeCorsRequest() {
+  var url = 'http://www.zdf.de/api/v2/content/p12:37951336';
 
-xhr.onerror = function() {
-  // Error code goes here.
-};
+  var xhr = createCORSRequest('GET', url);
+  if (!xhr) {
+    console.log('CORS not supported');
+    return;
+  }
 
-xhr.send();
+  // Response handlers.
+  xhr.onload = function() {
+    XMLdata = $(xhr.responseText)
+    var Treffer = XMLdata.find("treffer");
+
+    Treffer.find("teaser").each(function () {
+      $article = $('<div class="article"></div>');
+      $article.append(  "<h2>" + $(this).find("dachzeile").text() + "</h2>" +
+                        "<h3>" + $(this).find("teaserTitel").text() + "</h3>" +
+                        "<span>" + $(this).find("datum").text() + "</span>" + 
+                        "<p>" + $(this).find("teaserText").text() + "</p>" +
+                        "<a href=''>Zum Artikel gehen</a>" );
+
+      $article.append( "<br/>" );
+      $(".articles").append($article);
+      console.log($(this).find("datum").text());
+      console.log($(this).find("teaserText").text());
+
+    });
+  };
+
+  xhr.onerror = function() {
+    console.log('Woops, there was an error making the request.');
+  };
+
+  xhr.send();
+
+}
+
+makeCorsRequest(); 
+
+
