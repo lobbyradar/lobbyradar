6c9f7603b03ce073c16d64169ed630f9c7efe909
yetzt <git@yetzt.me>
2015-04-30 16:56:04 +0200

    fix links to live domain

--- assets/js/networkviz.js
+++ assets/js/networkviz.js
@@ -88,7 +88,7 @@ var NetworkViz = (function () {
 		map.on('mousemove', mousemove);
 		map.on('click', mouseclick);
 
-		var layer = L.tileLayer('http://lobbyradar.opendatacloud.de/lobbynetwork/tiles/{z}/{y}/{x}.png', {
+		var layer = L.tileLayer('https://www.lobbyradar.de/lobbynetwork/tiles/{z}/{y}/{x}.png', {
 			minZoom: 0,
 			maxZoom: 7,
 			maxNativeZoom: 7,
