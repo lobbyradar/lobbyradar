4b308ebf06dc14d051c1a4bb26e2f804f37949a7
ffalt <ffalt@datenfreunde.com>
2015-06-18 15:08:34 +0200

    import in-the-wild-fields to station fields list

---
+++ bin/checkfields.js
@@ -0,0 +1,83 @@
+var mongojs = require("mongojs");
+var debug = require("debug")("check fields");
+var async = require("async");
+var path = require("path");
+var fs = require("fs");
+var utils = require("../lib/utils.js");
+
+// load config
+var config = require(path.resolve(__dirname, "../config.js"));
+
+// load mongojs
+var db = mongojs(config.db, ["entities", "relations", "dataindex"]);
+
+// local modules
+var api = require(path.resolve(__dirname, "../lib/api.js"))(config.api, db);
+
+var fixFields = function (cb) {
+	console.log('Fixing Fields');
+
+	var fieldtypes = {};
+
+	var toList = function (obj) {
+		return Object.keys(obj).map(function (key) {
+			return obj[key];
+		}).sort(function (a, b) {
+			if (a.mode < b.mode)return -1;
+			if (a.mode > b.mode)return 1;
+			if (a.format < b.format)return -1;
+			if (a.format > b.format)return 1;
+			if (a.key < b.key)return -1;
+			if (a.key > b.key)return 1;
+			if (a.name < b.name)return -1;
+			if (a.name > b.name)return 1;
+			return 0;
+		})
+	};
+
+	var logEntityFieldType = function (d, ent) {
+		var mode = ent.type == 'person' ? 'persons' : 'organisations';
+		var id = [d.key, d.format, d.desc, mode].join('|');
+		fieldtypes[id] = {mode: mode, format: d.format, key: d.key, name: d.desc}
+	};
+
+	var logRelationFieldType = function (d) {
+		var id = [d.key, d.format, d.desc, 'relations'].join('|');
+		fieldtypes[id] = {mode: 'relations', format: d.format, key: d.key, name: d.desc}
+	};
+
+	api.field_list(null, function (err, fields) {
+		api.ents(function (err, ents) {
+			ents.forEach(function (ent) {
+				ent.data.forEach(function (d) {
+					logEntityFieldType(d, ent);
+				});
+			});
+			api.rels_full({full: true}, function (err, rels) {
+				rels.forEach(function (rel) {
+					rel.data.forEach(logRelationFieldType);
+				});
+				var fields_in_use = toList(fieldtypes);
+				var new_fields = fields_in_use.filter(function (t) {
+					//console.log(JSON.stringify(t));
+					var flist = fields.filter(function (f) {
+						return ((f.key == t.key) && (f.mode == t.mode) && (f.format == t.format) && (f.name == t.name));
+					});
+					return (flist.length == 0);
+				});
+				if (!new_fields.length) return cb();
+				console.log(new_fields.length, 'new fields');
+				async.forEachSeries(new_fields, function (t, next) {
+						console.log('create field', JSON.stringify(t));
+						api.field_create(t, next);
+					}, cb
+				);
+			});
+		});
+	});
+};
+
+fixFields(function () {
+	console.log('done');
+	process.exit();
+});
--- bin/cleanup.js
+++ bin/cleanup.js
@@ -689,7 +689,6 @@ var fixTitles = function (field, change_list) {
 	}
 };
 
-
 var fixEntities = function (cb) {
 	console.log('Fixing Entities');
 	api.ents(function (err, ents) {
@@ -802,70 +801,6 @@ var clone = function (obj) {
 	return JSON.parse(JSON.stringify(obj));
 };
 
-var fixFields = function (cb) {
-	console.log('Fixing Fields');
-
-	var fieldtypes = {};
-
-	var toList = function (obj) {
-		return Object.keys(obj).map(function (key) {
-			return obj[key];
-		}).sort(function (a, b) {
-			if (a.mode < b.mode)return -1;
-			if (a.mode > b.mode)return 1;
-			if (a.format < b.format)return -1;
-			if (a.format > b.format)return 1;
-			if (a.key < b.key)return -1;
-			if (a.key > b.key)return 1;
-			if (a.name < b.name)return -1;
-			if (a.name > b.name)return 1;
-			return 0;
-		})
-	};
-
-	var logEntityFieldType = function (d, ent) {
-		var mode = ent.type == 'person' ? 'persons' : 'organisations';
-		var id = [d.key, d.format, d.desc, mode].join('|');
-		fieldtypes[id] = {mode: mode, format: d.format, key: d.key, name: d.desc}
-	};
-
-	var logRelationFieldType = function (d) {
-		var id = [d.key, d.format, d.desc, 'relations'].join('|');
-		fieldtypes[id] = {mode: 'relations', format: d.format, key: d.key, name: d.desc}
-	};
-
-	api.field_list(null, function (err, fields) {
-		api.ents(function (err, ents) {
-			ents.forEach(function (ent) {
-				ent.data.forEach(function (d) {
-					logEntityFieldType(d, ent);
-				});
-			});
-			api.rels_full({full: true}, function (err, rels) {
-				rels.forEach(function (rel) {
-					rel.data.forEach(logRelationFieldType);
-				});
-				var fields_in_use = toList(fieldtypes);
-				var new_fields = fields_in_use.filter(function (t) {
-					//console.log(JSON.stringify(t));
-					var flist = fields.filter(function (f) {
-						return ((f.key == t.key) && (f.mode == t.mode) && (f.format == t.format) && (f.name == t.name));
-					});
-					return (flist.length == 0);
-				});
-				if (!new_fields.length) return cb();
-				//console.log(new_fields.length, 'new fields');
-				//async.forEachSeries(new_fields, function (t, next) {
-				//		console.log('create field', JSON.stringify(t));
-				//		api.field_create(t, next);
-				//	}, cb
-				//);
-				cb();
-			});
-		});
-	});
-};
-
 fixEntities(function (entities) {
 	fixRelations(entities, function () {
 		if (list.length > 0) {
@@ -873,16 +808,13 @@ fixEntities(function (entities) {
 			var filename = './cleanup-log-' + (new Date()).valueOf() + '.json';
 			fs.writeFileSync(path.resolve(__dirname, filename), JSON.stringify(list, null, '\t'));
 		}
-		titles.sort(function (a, b) {
-			if (a < b)return -1;
-			if (a > b)return 1;
-			return 0;
-		});
-		console.log(titles);
-
-		fixFields(function () {
-			console.log('done');
-			process.exit();
-		});
+		//titles.sort(function (a, b) {
+		//	if (a < b)return -1;
+		//	if (a > b)return 1;
+		//	return 0;
+		//});
+		//console.log(titles);
+		console.log('done');
+		process.exit();
 	});
 });
